60c62948e1b90cf1c2aaad1cf6730d044177214a
George Jose <george.jose.a@gmail.com>
2017-07-29 22:05:44 -0400

    - Upgraded to work on python3 & macOS 10.12 - Simplified code

--- AmbienceHue.py
+++ AmbienceHue.py
@@ -1,164 +1,105 @@
 import time
 import struct
-import collections
-from phue import Bridge
 import Quartz.CoreGraphics as CG
+import numpy as np
+from phue import Bridge
 import colorsys
+import time
 
-BRIDGE_IP = '192.168.0.10'
-LEFT_LIGHT = 3
-RIGHT_LIGHT  = 1
-# LEFT_LIGHT = 3
-# RIGHT_LIGHT  = 2
-TRANSITION_TIME = 0.1
+import contextlib
 
-class ScreenPixel(object):
+@contextlib.contextmanager
+def timer(msg):
+    start = time.time()
+    yield
+    end = time.time()
+    print("Framerate:\t", str(1.0 / (end - start)))
+    print("Time:\t", str(end - start))
+
+BRIDGE_IP = '10.88.111.1'
+
+class Screen:
     def capture(self, region = None):
-        if region is None:
+        if region == None:
             region = CG.CGRectInfinite
         else:
-            
-            if region.size.width % 2 > 0:
-                emsg = "Capture region width should be even (was %s)" % (
-                    region.size.width)
-                raise ValueError(emsg)
-
-        image = CG.CGWindowListCreateImage(
-            region,
-            CG.kCGWindowListOptionOnScreenOnly,
-            CG.kCGNullWindowID,
-            CG.kCGWindowImageDefault)
-
-        prov = CG.CGImageGetDataProvider(image)
-
-        self._data = CG.CGDataProviderCopyData(prov)
-
+            region = CG.CGRectMake(region)
+        image = CG.CGWindowListCreateImage(region, CG.kCGWindowListOptionOnScreenOnly, CG.kCGNullWindowID, CG.kCGWindowImageDefault)
+        provider = CG.CGImageGetDataProvider(image)
+        self._data = CG.CGDataProviderCopyData(provider)
         self.width = CG.CGImageGetWidth(image)
         self.height = CG.CGImageGetHeight(image)
+        imgdata=np.fromstring(self._data,dtype=np.uint8).reshape(len(self._data)/4,4)
+        return imgdata[:self.width*self.height,:-1].reshape(self.height,self.width,3)
 
-    def pixel(self, x, y):
+def rgb2hsv(color):
+    r, g, b = color
+    _h, _s, _v = colorsys.rgb_to_hsv(r/255, g/255, b/255)
+    h = int(_h * 65535)
+    s = int(_s * 255)
+    v = int(_v * 255)
+    return h, s, v
 
-
-
-        data_format = "BBBB"
-
-
-        offset = 4 * ((self.width*int(round(y))) + int(round(x)))
-
-
-        b, g, r, a = struct.unpack_from(data_format, self._data, offset=offset)
-        color = collections.namedtuple('color', ['r', 'g', 'b', 'a'])
-        c = color(r = r, g = g, b = b, a = a)
-        return c
-
-
-if __name__ == '__main__':
-    # Timer helper-function
-    import contextlib
-
-    @contextlib.contextmanager
-    def timer(msg):
-        start = time.time()
-        yield
-        end = time.time()
-        # print "%s: %.02fs" % (msg, (end-start))
-        print "%s: %.02ffps" % ("Framerate:\t", 1.0/(end-start))
-        print "%s: %.02fs" % (msg, (end-start))
-
-    b = Bridge(BRIDGE_IP)
-    b.connect()
-    b.set_light(LEFT_LIGHT+1, 'on', True)
-    b.set_light(RIGHT_LIGHT+1, 'on', True)
-    lights = b.lights
-    lights[LEFT_LIGHT].transitiontime = TRANSITION_TIME
-    lights[RIGHT_LIGHT].transitiontime = TRANSITION_TIME
-
-    lights[LEFT_LIGHT].on = True
-    lights[RIGHT_LIGHT].on = True
-
-    sp = ScreenPixel()
-    sp.capture()
-
-    COL_INTERVAL = 50
-    ROW_INTERVAL = 100
-    WIDTH = sp.width/20
-    HEIGHT = sp.height
-    print "---PARAMETERS---"
-    print "width: \t", WIDTH
-    print "height: \t", HEIGHT
-    print "Rows being processed:\t", int(HEIGHT / ROW_INTERVAL)
-    print "Cols being processed:\t", int(WIDTH / COL_INTERVAL)
-    print "Pixels being processed:\t", int(WIDTH / COL_INTERVAL) * int(HEIGHT / ROW_INTERVAL)
+class Hue:
+    def __init__(self, ip):
+        self.b = Bridge(ip)
+        self.b.connect()
+        self.RIGHT = 1
+        self.LEFT = 2
+        self.TRANSITION_TIME = 0
+        self.b.lights[self.LEFT].transitiontime = self.TRANSITION_TIME
+        self.b.lights[self.RIGHT].transitiontime = self.TRANSITION_TIME
+        self.blink(self.LEFT)
+        self.blink(self.RIGHT)
+        self.debug()
     
-    i = 0
-    while True:
-        ++i
-        # if (i%100 == 0): 
-        #     b.connect()
-        # with timer ("Capture"):
-        sp.capture()
-        avg_r = 0
-        avg_g = 0
-        avg_b = 0
-        with timer("Computation time:\t"): 
+    def debug(self):
+        for i, light in enumerate(self.b.lights):
+            print(light)
 
-            avg_r = 0
-            avg_g = 0
-            avg_b = 0
-            for col in range(WIDTH):
-                for row in range(HEIGHT):
-                    if row % ROW_INTERVAL == 0 and col % COL_INTERVAL == 0:
-                        # print(sp.pixel(row, col))
-                        avg_r += sp.pixel(col, row).r
-                        avg_g += sp.pixel(col, row).g
-                        avg_b += sp.pixel(col, row).b
-                        # print (row, col, avg_r)
-            avg_r = avg_r / (WIDTH * HEIGHT / COL_INTERVAL / ROW_INTERVAL)
-            avg_g = avg_g / (WIDTH * HEIGHT / COL_INTERVAL / ROW_INTERVAL)
-            avg_b = avg_b / (WIDTH * HEIGHT / COL_INTERVAL / ROW_INTERVAL)
-            h, s, v = colorsys.rgb_to_hsv((avg_r+0.0)/255, (avg_g+0.0)/255, (avg_b+0.0)/255)
-            command = {'hue': int(h * 65535), 'sat': int(s * 255), 'bri': int(v * 255)}
-            
-            # print "\nLeft HSV: ", h, s, v
-            # print "Left RGB: ", avg_r, avg_g, avg_b
-            b.set_light(LEFT_LIGHT+1, command)
+    def off(self, light):
+        self.b.set_light(light, 'on', False)
 
-            avg_r = 0
-            avg_g = 0
-            avg_b = 0
-            for col in range(sp.width - WIDTH, sp.width):
-                    for row in range(HEIGHT):
-                        if row % ROW_INTERVAL == 0 and col % COL_INTERVAL == 0:
-                            # print(sp.pixel(row, col))
-                            avg_r += sp.pixel(col, row).r
-                            avg_g += sp.pixel(col, row).g
-                            avg_b += sp.pixel(col, row).b
-                            # print (row, col, avg_r)
-            avg_r = avg_r / (WIDTH * HEIGHT / COL_INTERVAL / ROW_INTERVAL)
-            avg_g = avg_g / (WIDTH * HEIGHT / COL_INTERVAL / ROW_INTERVAL)
-            avg_b = avg_b / (WIDTH * HEIGHT / COL_INTERVAL / ROW_INTERVAL)
-            h, s, v = colorsys.rgb_to_hsv((avg_r+0.0)/255, (avg_g+0.0)/255, (avg_b+0.0)/255)
-            command = {'hue': int(h * 65535), 'sat': int(s * 255), 'bri': int(v * 255)}
-            
-            # print "\nRight HSV: ", h, s, v
-            # print "Right RGB: ", avg_r, avg_g, avg_b
-            b.set_light(RIGHT_LIGHT+1, command)
+    def on(self, light):
+        self.b.set_light(light, 'on', True)
 
+    def set_color_hsv(self, light, color):
+        h, s, v = color
+        command = {'hue': (h), 'sat': (s), 'bri': (v)}
+        self.b.set_light(light, command)
 
+    def set_color_rgb(self, light, color):
+        self.set_color_hsv(light, rgb2hsv(color))
 
+    def blink(self, light, times = 1):
+        for i in range(times):
+            self.off(light)
+            time.sleep(0.5)
+            self.on(light)
+            time.sleep(0.5)
 
+h = Hue(BRIDGE_IP)
+h.set_color_rgb(1, (255, 255, 255))
 
+sp = Screen()
 
-
-
-
-
-
-
-
-
-
-
-
+while True:
+    try:
+        img = sp.capture()
+        
+        WIDTH = 50
+        left = img[:, 0:WIDTH, :]
+        right = img[:, -WIDTH:, :]
+        lr, lg, lb = np.mean(left[:, :, 0]), np.mean(left[:, :, 1]), np.mean(left[:, :, 2])
+        rr, rg, rb = np.mean(right[:, :, 0]), np.mean(right[:, :, 1]), np.mean(right[:, :, 2])
+        h.set_color_rgb(h.LEFT, (lr, lg, lb))
+        h.set_color_rgb(h.RIGHT, (rr, rg, rb))
+    except Exception:
+        print('exception')
+        pass
+    except KeyboardInterrupt:
+        h.set_color_rgb(h.LEFT, (255, 255, 255))
+        h.set_color_rgb(h.RIGHT, (255, 255, 255))
+        exit()
 
 
